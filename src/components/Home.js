3fb2b2d6739d4bf31888efdd558d7efdea2ab0a7
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2021-12-26 10:25:00 +0530

    Adding Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,34 @@
+import { Button, Container, Image, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
+    const ImgStyle = {
+        width: '100%',
+        height: '400px',
+        position: 'absolute',
+        top: '60px',
+        left: 0,
+        zIndex: 1
+    }
+
+    const TextStyle = {
+        position: 'absolute',
+        zIndex: 2,
+        top: '100px',
+        left: '50px'
+    }
     return (
         <div>
+            <Image src='https://images.pexels.com/photos/317356/pexels-photo-317356.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' style={ImgStyle} />
+            <Container style={TextStyle}>
+                <Text fontSize='4xl'>Write Your First Note in Notes Taker.</Text>
+                <Link to='/new'>
+                    <Button colorScheme='blue' marginY={5}>
+                        Get Started
+                    </Button>
+                </Link>
+            </Container>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Box, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <Box bg='blue.500' w='100%' p={4} color='white'>
-            <Text fontSize='xl'>Notes Taker</Text>
+            <Link to='/'>
+                <Text fontSize='xl'>Notes Taker</Text>
+            </Link>
         </Box>
     )
 }
